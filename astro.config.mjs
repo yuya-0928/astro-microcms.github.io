ea135f3680031c56c09bd468515848b9a245e743
yuya-0928 <hirako.yuuya0928.4620@gmail.com>
2025-03-14 02:07:28 +0900

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  site: 'https://yuya-0928.github.io',
+  base: '/sandbox-astro-satellite-spiral',
+  output: 'static'
 });
